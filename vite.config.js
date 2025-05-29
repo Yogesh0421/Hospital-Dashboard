f760479061bfbfee9b6ebf27299b4db4c161a14c
Yogesh Sahu <cs20.yogesh.sahu@lcit.edu.in>
2025-05-29 21:15:55 +0530

    first commit

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,8 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  base: '/Hospital-Dashboard/', 
+  base: '/Hospital-Dashboard',
   plugins: [react()],
 });
+
 
