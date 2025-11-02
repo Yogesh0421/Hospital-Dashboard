006a0b3f40c055734a22958e03b916f8f53e1c79
Yogesh Sahu <cs20.yogesh.sahu@lcit.edu.in>
2025-11-02 17:04:47 +0530

    config file updated

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,15 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
+// this is for gh pages
+// export default defineConfig({
+//   base: '/Hospital-Dashboard',
+//   plugins: [react()],
+// });
+
+// this is for vercel app
 export default defineConfig({
-  base: '/Hospital-Dashboard',
+  base: './',
   plugins: [react()],
 });
 
